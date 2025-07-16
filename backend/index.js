bd05c9226bd568e1811d762235d04fc9cfa4d3ef
Mirac Erarslan <imposterdeveloper34@gmail.com>
2025-07-16 23:24:25 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,16 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://game-i1y4ko7f2-imposters-projects-870766c7.vercel.app',
+        'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.use(express.json());
 
 const supabase = createClient(
